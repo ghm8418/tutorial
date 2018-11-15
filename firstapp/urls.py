f7779b12d757306059139b5e11620bd463787958
ghm8418 <ghm8418@gmail.com>
2018-11-15 15:37:03 +0900

    let's git

---
+++ firstapp/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from firstapp import views
+
+urlpatterns = [
+ path('main/', views.main, name='main'),
+ path('show/', views.show, name='show')]
